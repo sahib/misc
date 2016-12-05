20709205d408cf68479db4a16f40dda7ddbde6aa
Chris P <sahib@online.de>
2016-12-05 17:49:26 +0100

    Initial working version, finetuning and deployment still missing

---
+++ imageloader/loader.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+# encoding: utf8
+
+'''Small image downloading utilty.
+
+Usage:
+
+    $ python image-downloader.py [list-of-urls]
+'''
+
+import os
+import sys
+import socket
+import logging
+import hashlib
+import urllib.request
+
+from urllib.parse import urlparse
+from concurrent.futures import ThreadPoolExecutor
+
+
+def read_urllist(path):
+    'Read a list of urls from the file at `path`'
+    try:
+        with open(path, 'r') as handle:
+            return parse_urllist(handle)
+    except OSError as err:
+        logging.warning('Failed to open %s: %s', path, err)
+        return []
+
+
+def parse_urllist(handle):
+    'Parses a list of urls from the file descriptor pointed to by `handle`'
+    # Skip empty lines, if any; might also filter comments here:
+    return list(filter(None, map(str.strip, handle)))
+
+
+def basename_from_url(url):
+    'Attempts to guess the basename of the saved file from the image url'
+    parsed = urlparse(url)
+    *_, last = os.path.split(parsed.path)
+    if len(last) == 0:
+        raise ValueError('Empty URL: ' + url)
+
+    # Watch out for things like '..' and '.' if present.
+    # Might become a problem if joined to a path.
+    if last == '.' or last == '..':
+        raise ValueError('Incompatible path')
+
+    return last
+
+
+def save_image(basedir, url, content):
+    'Save `content` from `url` to the images/ subdir of `basedir`'
+    # Deduplicate the downloaded images automatically:
+    suffix = hashlib.sha256(content).hexdigest()
+    data_path = os.path.join(basedir, 'images/objects', suffix)
+    link_path = os.path.join(basedir, 'images/names', basename_from_url(url))
+
+    try:
+        os.makedirs(os.path.join(basedir, 'images/objects'), exist_ok=True)
+        os.makedirs(os.path.join(basedir, 'images/names'), exist_ok=True)
+
+        with open(data_path, 'wb') as handle:
+            handle.write(content)
+    except OSError as err:
+        logging.warning('Failed to save %s to %s: %s', url, data_path, err)
+        return
+
+    # Hardlinks are not very portable, but I assume it runs on Linux:
+    try:
+        os.link(data_path, link_path)
+    except FileExistsError:
+        pass
+
+
+def download_image(url, timeout=5):
+    'Attempts to download the image at `url`, waiting at most `timeout` secs'
+    try:
+        response = urllib.request.urlopen(url, timeout=timeout)
+    except ValueError:
+        logging.warning('Bad URL: ' + url)
+        return '', None
+    except (socket.timeout, urllib.error.HTTPError, urllib.error.URLError):
+        return url, None
+
+    try:
+        data = response.read()
+    except OSError as err:
+        logging.warning('Failed to download `%s`: %s', url, err)
+
+    return url, data
+
+
+def download_images(urls):
+    'Downloads all images in `urls` and saves them to local storage'
+    # Images may be large, so watch out to keeping the parallel downloads low.
+    # If this poses to be a problem, one might also stream the image directly
+    # to the filesystem without much buffering in memory.
+    cwd = os.getcwd()
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        for (url, content) in executor.map(download_image, urls):
+            if content is not None:
+                save_image(cwd, url, content)
+
+
+def main():
+    'Handle the very primitive commandline interface'
+    if len(sys.argv) < 2:
+        print(__doc__)
+        sys.exit(1)
+
+    urls = read_urllist(sys.argv[1])
+    if len(urls) > 0:
+        download_images(urls)
+
+
+if __name__ == '__main__':
+    main()
