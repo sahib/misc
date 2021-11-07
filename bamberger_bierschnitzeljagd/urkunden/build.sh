3a29dfecc01e52427a8cd10e89fa41b533ecbb0f
Chris Pahl <sahib@online.de>
2021-11-07 12:58:39 +0100

    bamberg: should be fine now

--- bamberger_bierschnitzeljagd/urkunden/build.sh
+++ bamberger_bierschnitzeljagd/urkunden/build.sh
@@ -6,9 +6,14 @@ set -x
 compile_with_include() {
     local include_path="$1"
     local output_dir="pdfs/$(basename ${1/.inc/})"
+    local output_pdf="pdfs/$(basename ${1/.inc/}).pdf"
+
     sed "s#<<PLACEHOLDER>>#${include_path}#g" urkunde.tex > urkunde.tmp
     latexmk -gg -xelatex -outdir="$output_dir" urkunde.tmp
     rm urkunde.tmp
+
+    mv "$output_dir/urkunde.pdf" "$output_pdf"
+    rm -rf "$output_dir"
 }
 
 if [ -z "${1:-}" ]; then
